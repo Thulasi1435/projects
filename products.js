e869883e88cbab075ade06254e09a1256d6f819d
Thulasi1435 <vendruthulasi@gmail.com>
2025-03-12 19:20:01 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,34 @@
+[
+    {
+        "id": 1,
+        "name": "iPhone 14",
+        "brand": "Apple",
+        "price": 999,
+        "image": "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6505/6505124_sd.jpg",
+        "description": "Latest iPhone model with A15 Bionic chip"
+    },
+    {
+        "id": 2,
+        "name": "Galaxy S22",
+        "brand": "Samsung",
+        "price": 799,
+        "image": "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6472/6472835_sd.jpg",
+        "description": "Samsung flagship with excellent camera"
+    },
+    {
+        "id": 3,
+        "name": "OnePlus 9",
+        "brand": "OnePlus",
+        "price": 699,
+        "image": "https://tse3.mm.bing.net/th?id=OIP.W9o6-bHR1otieOR9-kCXAgHaJB&pid=Api&P=0&h=180",
+        "description": "Fast charging and smooth performance"
+    }, 
+    {            
+      "id": 4,
+      "name": "Realme 9i 5g",
+      "brand": "realme",
+      "price": 800,
+      "image": "https://specs-arena.com/wp-content/uploads/2023/11/Realme-9i-5G.webp",
+      "description": "Fast charging and smooth performance" 
+}
+]
